467bf43d4b4b191486e288a4ba93348738b834a5
egamasa <40469431+egamasa@users.noreply.github.com>
2024-10-30 23:51:33 +0900

    frontend / 絵文字表示

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,11 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        emoji: ['Noto Color Emoji', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
-
